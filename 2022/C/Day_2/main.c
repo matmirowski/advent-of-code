64df80fb1addd7c5007f6297c040fc572a2e8f5d
Mateusz Mirowski <mateusz.j.mirowski@gmail.com>
2022-12-26 16:45:06 +0100

    Finish day 2 in C

--- 2022/C/Day_2/main.c
+++ 2022/C/Day_2/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    char line[10];
+    char line[5];
     int pointsPart1 = 0;
     int pointsPart2 = 0;
     FILE *file = fopen("input.txt", "r");
@@ -12,15 +12,70 @@ int main() {
     }
 
     while (fgets(line, sizeof(line), file)) {
+
         // part 1
-        
         char enemyPick = line[0];
         char myPick = line[2];
 
         switch (myPick) {
+            case 'X': // rock
+                pointsPart1 += 1;
+                if (enemyPick == 'A')
+                    pointsPart1 += 3;
+                else if (enemyPick == 'C')
+                    pointsPart1 += 6;
+                break;
+            case 'Y': // paper
+                pointsPart1 += 2;
+                if (enemyPick == 'B')
+                    pointsPart1 += 3;
+                else if (enemyPick == 'A')
+                    pointsPart1 += 6;
+                break;
+            case 'Z': // scissors
+                pointsPart1 += 3;
+                if (enemyPick == 'C')
+                    pointsPart1 += 3;
+                else if (enemyPick == 'B')
+                    pointsPart1 += 6;
+                break;
+        }
 
+        // part 2
+        switch (myPick) {
+            case 'X':
+                if (enemyPick == 'A')
+                    pointsPart2 += 3;
+                else if (enemyPick == 'B')
+                    pointsPart2 += 1;
+                else
+                    pointsPart2 += 2;
+                break;
+            case 'Y':
+                if (enemyPick == 'A') {
+                    pointsPart2 += 4; // 3 + 1
+                }
+                else if (enemyPick == 'B') {
+                    pointsPart2 += 5; // 3 + 2
+                }
+                else if (enemyPick == 'C'){ // C
+                    pointsPart2 += 6; // 3 + 3
+                }
+                break;
+            case 'Z':
+                pointsPart2 += 6;
+                if (enemyPick == 'A')
+                    pointsPart2 += 2;
+                else if (enemyPick == 'B')
+                    pointsPart2 += 3;
+                else
+                    pointsPart2 += 1;
+                break;
         }
     }
     fclose(file);
+
+    printf("%d\n", pointsPart1);
+    printf("%d", pointsPart2);
     return 0;
 }
